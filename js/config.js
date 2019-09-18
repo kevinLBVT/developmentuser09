01fd5d1b5d35fe615421286658e70e0bc5511ce8
kevinLBVT <54675464+kevinLBVT@users.noreply.github.com>
2019-09-18 15:23:21 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-1_W2ynQQhwZ
-        userPoolClientId: '', // e.g. 4msu1l9d2ssv9kd9fr3h8satn8
-        region: '' // e.g. us-east-2
+        userPoolId: '', // e.g. us-east-1_OsNF1d8Xb
+        userPoolClientId: '', // e.g. 1dqj6j03dlio34q69fafmak0c0
+        region: '' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
